e3b8f45c754d130bb5361ae2bc7661e999f3f266
calvo111979 <acalvoinfa10@gmail.com>
2021-04-12 11:48:33 +0200

    feat:muestra el logo de biko

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("muestra el logo de biko", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const logoBiko = screen.getByRole("img", { name: "Logo de Biko" });
+  expect(logoBiko).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import LogoBiko from "./logo-biko.svg";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <img alt="Logo de Biko" src={LogoBiko} />
+      </header>
     </div>
   );
 }
