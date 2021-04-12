9e66352097a5e7fd7758104a7fad144d5ba0dabc
calvo111979 <acalvoinfa10@gmail.com>
2021-04-12 17:26:13 +0200

    refactor: elimina test innecesarios

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,22 +2,12 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("muestra el logo de biko", () => {
-  render(<App />);
-  const logoBiko = screen.getByRole("img", { name: "Logo de Biko" });
-  expect(logoBiko).toBeInTheDocument();
-});
-
-test("muestra el título de los pasos", () => {
-  render(<App />);
-  const pathTitle = screen.getByText(/pasos del learning path/i);
-  expect(pathTitle).toBeInTheDocument();
-});
-
-test("muestra una kata completa sin desplegar", () => {
-  render(<App />);
-  const kataTitle = screen.getByText(/kata roman numerals - en solitario/i);
-  const kataState = screen.getByRole("img", { name: /estado completado/i });
-  expect(kataTitle).toBeInTheDocument();
-  expect(kataState).toBeInTheDocument();
+describe("listado de pasos", () => {
+  it("muestra una kata", () => {
+    render(<App />);
+    const kataTitle = screen.getByText(/kata roman numerals - en solitario/i);
+    const kataState = screen.getByRole("img", { name: /estado completado/i });
+    expect(kataTitle).toBeInTheDocument();
+    expect(kataState).toBeInTheDocument();
+  });
 });
