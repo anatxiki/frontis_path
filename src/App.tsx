3a19a1e55b233de357f888cd6a4f8089a16fc808
Marcos Galvez <marcos.galvez@biko2.com>
2021-04-13 14:42:53 +0200

    Agrega cuadrado titulo y retoques body

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import LogoBiko from "./logo-biko.svg";
+import RedSquare from "./red-square.svg";
 import { font } from "./ui/theme";
 import styled from "styled-components";
 import { Kata } from "./Kata";
 import { katas } from "./katas.json";
+import { rem } from "polished";
 
 const H1 = styled.h1`
   ${font.h1()}
 `;
 
+const Logo = styled.img`
+  max-width: ${rem(392)};
+`;
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img alt="Logo de Biko" src={LogoBiko} />
+        <Logo alt="Logo de Biko" src={LogoBiko} />
+        <img src={RedSquare} />
         <H1>Pasos del learning path</H1>
       </header>
       {katas.map((element: any, idx: number) => {
--- src/ui/GlobalStyles/GlobalStyles.ts
+++ src/ui/GlobalStyles/GlobalStyles.ts
@@ -1,12 +1,15 @@
 import { createGlobalStyle } from "styled-components";
-import { fontFamily } from "../theme";
+import { fontFamily, grid } from "../theme";
 import { reboot } from "./reboot";
 import { reset } from "./reset";
+import { rem } from "polished";
 
 export const GlobalStyles = createGlobalStyle`
     ${reset};
     ${reboot};
     body{
         font-family: ${fontFamily};
+        margin: ${rem(0)} ${rem(grid.gap.desktop)};
+        max-width: ${rem(grid.contentMaxWidth)};
     }    
 `;
