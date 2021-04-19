fcb1c21ebb3b86998bc30a3178554d994311f66a
Marcos Galvez <marcos.galvez@biko2.com>
2021-04-19 15:11:00 +0200

    feat: restaura logo biko

--- src/Steps.tsx
+++ src/Steps.tsx
@@ -1,4 +1,4 @@
-//import LogoBiko from "./material/logo-biko.svg";
+import LogoBiko from "./material/logo-biko.svg";
 import RedSquare from "./material/red-square.svg";
 import { font, size } from "./ui/theme";
 import styled from "styled-components";
@@ -11,7 +11,7 @@ export function Steps() {
   return (
     <>
       <header>
-        {/*<Logo alt="Logo de Biko" src={LogoBiko} />*/}
+        <Logo alt="Logo de Biko" src={LogoBiko} />
         <TitleWrapper>
           <img src={RedSquare} alt="Cuadrado rojo decorativo" />
           <H1>Pasos del learning path</H1>
@@ -32,10 +32,10 @@ const H1 = styled.h1`
   margin-left: ${rem(-130)};
 `;
 
-/*const Logo = styled.img`
-    max-width: ${rem(150)};
-    margin-top: ${rem(size.base)};
-  `;*/
+const Logo = styled.img`
+  max-width: ${rem(150)};
+  margin-top: ${rem(size.base)};
+`;
 
 const TitleWrapper = styled.div`
   margin: ${rem(size.medium)} 0;
